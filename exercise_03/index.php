ad340fdc22d56d19ddd0eb46c506a89a9b1674f2
daniel sinteck <d.sint3ck@gmail.com>
2024-10-29 17:51:30 +0100

    ++PHP arithmetic explained;

---
+++ exercise_03/index.php
@@ -0,0 +1,42 @@
+<!-- PHP arithmetic explained -->
+
+<?php
+// Arithmetic operators
+// + - * / ** %
+
+// $x = 10;
+// $y = 3;
+// $z = null;
+
+// $z = $x + $y;
+// $z = $x - $y;
+// $z = $x * $y;
+// $z = $x / $y;
+// $z = $x ** $y;
+// $z = $x % $y;
+// echo $z;
+
+/////////////////////////////
+
+// Increment / Decrement operators
+// ++ --
+
+// $counter = 0;
+// $counter++;
+// $counter--;
+// $counter += 5;
+// $counter -= 5;
+// echo $counter;
+
+/////////////////////////////
+
+// Operator Precedence
+// ()
+// **
+// * / %
+// + -
+
+// $total = 10 + 5 * 3 ** 2 / 4;
+// echo $total;
+
+?>
