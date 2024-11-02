4bd33200a7321ca5b6693eec9c63fd9bdfa12db4
daniel sinteck <d.sint3ck@gmail.com>
2024-11-02 17:09:52 +0100

    ++ PHP logical operators theory1;

---
+++ exercise_07/index.php
@@ -0,0 +1,10 @@
+<!-- PHP logical operators 
+
+ // Logical operators = combine conditional statements
+ // if(condition1 && condition2)
+
+ // && = True if both conditions are true
+ // || = True if at least one condition is true
+ // ! = True if false. False if true.
+-->
+
