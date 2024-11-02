5a6882afc32788bcb82af712c30d562f305ba713
daniel sinteck <d.sint3ck@gmail.com>
2024-11-02 17:21:11 +0100

    ++ PHP logical operators theory2;

--- exercise_07/index.php
+++ exercise_07/index.php
@@ -6,5 +6,29 @@
  // && = True if both conditions are true
  // || = True if at least one condition is true
  // ! = True if false. False if true.
--->
+--> 
 
+<a href="./exercise.php">To the exercise</a><br><br>
+
+<?php
+$temp = 15;
+$cloudy = true;
+
+if ($temp >= 0 && $temp <= 30) {
+    echo "The weather is good. <br>";
+} else {
+    echo "The weather is bad. <br>";
+}
+
+if ($temp < 0 || $temp > 30) {
+    echo "The weather is bad. <br>";
+} else {
+    echo "The weather is good. <br>";
+}
+
+if ($cloudy) {
+    echo "It's sunny. <br>";
+} else {
+    echo "It's cloudy <br>";
+}
+?>
