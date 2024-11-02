c9a4f5fd685997b811729724a5fe175dbc03e244
daniel sinteck <d.sint3ck@gmail.com>
2024-11-02 17:40:28 +0100

    ++ PHP switches theory2;

--- exercise_08/index.php
+++ exercise_08/index.php
@@ -6,3 +6,27 @@ PHP switches
 -->
 
 <a href="./exercise.php">To the exercise</a><br><br>
+
+<?php
+$grade = "B";
+
+switch ($grade) {
+    case "A":
+        echo "You did great!";
+        break;
+    case "B":
+        echo "You did good!";
+        break;
+    case "C":
+        echo "You did okay!";
+        break;
+    case "D":
+        echo "You did poorly!";
+        break;
+    case "F":
+        echo "You failed!";
+        break;
+    default:
+        echo "{$grade} is not valid";
+}
+?>
