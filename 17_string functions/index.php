678cc3a11c262e50076d497d1d5e4ff1423d112d
daniel sinteck <d.sint3ck@gmail.com>
2024-11-06 22:25:15 +0100

    ++ last commit commends;

--- 17_string functions/index.php
+++ 17_string functions/index.php
@@ -5,20 +5,20 @@
 $username = "Bro Code";
     $phone = "123-456-7890";
 
-    //$username = strtolower($username);
-    //$username = strtoupper($username);
-    //$username = trim($username);
-    //$username = str_pad($username, 20, "0");
-    //$phone = str_replace("-", "", $phone);
-    //$username = strrev($username);
-    //$username = str_shuffle($username);
-    //$equals = strcmp($username, "Bro Code");
-    //$count = strlen($phone);
-    //$index = strpos($phone, "-");
-    //$firstname = substr($username, 0, 3);
-    //$lastname = substr($username, 4);
-    //$fullname = explode(" ", $username);
-    //$username = implode("-", $username);
+    //$username = strtolower($username); // strtolower() = lowercase
+    //$username = strtoupper($username); // strtoupper() = uppercase
+    //$username = trim($username); // trim() = remove whitespace
+    //$username = str_pad($username, 20, "0"); // str_pad() = add padding
+    //$phone = str_replace("-", "", $phone);    // str_replace() = replace
+    //$username = strrev($username); // strrev() = reverse
+    //$username = str_shuffle($username); // str_shuffle() = shuffle
+    //$equals = strcmp($username, "Bro Code");  // strcmp() = compare
+    //$count = strlen($phone); // strlen() = count
+    //$index = strpos($phone, "-"); // strpos() = find position
+    //$firstname = substr($username, 0, 3);     // substr() = substring
+    //$lastname = substr($username, 4);    // substr() = substring
+    //$fullname = explode(" ", $username); // explode() = split
+    //$username = implode("-", $username); // implode() = join
 
     echo $username;
 ?>
