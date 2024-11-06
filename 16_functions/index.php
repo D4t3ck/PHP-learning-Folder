e382326b3915da4e8b0a9a28e0d43b3f1075fd95
daniel sinteck <d.sint3ck@gmail.com>
2024-11-06 22:00:42 +0100

    ++ functions theory;

---
+++ 16_functions/index.php
@@ -0,0 +1,21 @@
+<!-- PHP functions
+
+// function = write some code, reuse when you need it
+// type() after function name to invoke
+// ex. add() substract() multiply() divide()
+-->
+
+<?php
+
+// function add($num1, $num2) {
+//     return $num1 + $num2;
+// }
+// echo add(10, 20);
+
+function is_even($number){
+    return $number % 2;
+}
+
+echo is_even(1);
+?>
+
